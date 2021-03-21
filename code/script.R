703289f490179d53186851e5dd8ba07cf51d7ad7
StatChin <chin.hiunvun@gmail.com>
2021-03-21 11:35:52 +0800

    R2

--- code/script.R
+++ code/script.R
@@ -1,1 +1,14 @@
 # script for analysis ####
+# load dummy data
+library(readxl)
+data<-read_excel('data/data.xlsx')
+head(data)
+str(data)
+data$cd<-as.factor(data$cd)
+data$as<-as.factor(data$as)
+data$sp<-as.factor(data$sp)
+head(data)
+summary(data$cd)
+summary(data$as)
+summary(data$sp)
+summary(data$rt_cd)
