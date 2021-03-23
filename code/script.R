68e6f5c808cb09a1ec7d07e26bd0a95fd96bf1da
StatChin <chin.hiunvun@gmail.com>
2021-03-24 00:27:17 +0800

    Add PCA and plot

--- code/script.R
+++ code/script.R
@@ -3,7 +3,7 @@
 # load packages needed in one go. You should have pacman and other packages installed beforehand. 
 pacman::p_load(pacman, dplyr, ggplot2, tidyr, rmarkdown, shiny, readxl, psych, rio, vegan, agricolae, ggpubr, haven)
 
-# load dummy data #####
+# load dummy data ####
 data<-import('data/data.xlsx') # rio package is needed
 head(data) # see the head of the data frame if corrected imported
 str(data) # see the structure of the data. Some data types may be transformed, such as from chr to factor
